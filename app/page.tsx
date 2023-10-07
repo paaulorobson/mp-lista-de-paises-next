089cf60af0f265ec1aa49a7108ff2796ce34d461
Paulo Robson <p.robson085@gmail.com>
2023-10-07 11:46:06 -0300

    feat: configurando projeto layout, adicionando pagina home

---
+++ app/page.tsx
@@ -0,0 +1,42 @@
+import Image from "next/image";
+
+type Country = {
+  name: {
+    common: string;
+  }
+  translations: {
+    por: {
+      common: string;
+    }
+  }
+  flags: {
+      svg: string;  
+      alt: string;
+    }
+}
+
+async function getCountries(): Promise<Country[]> {
+  const response = await fetch('https://restcountries.com/v3.1/all');
+  return response.json();
+}
+
+export default async function Home() {
+  // isso pode ser feito com o next (componente asyncrono)
+  const countries = await getCountries();
+
+  return ( 
+    <section className="container grid grid-cols-5 w-full gap-2 mt-16">
+      {countries.map(country => (
+        <article 
+          className="h-64 min-w-full p-2 bg-white border-2 rounded-xl hover:border-indigo-200 transition-all hover:shadow-xl cursor-pointer" 
+          key={country.name.common} 
+        > 
+          <div className="relative w-full h-40 p-2 overflow-hidden rounded-xl">
+            <Image src={country.flags.svg} alt={country.flags.alt} fill className="object-cover"/>
+          </div> 
+          <h1 className="font-bold text-xl text-center mt-1"> {country.translations.por.common}</h1>
+        </article>
+      ))}
+    </section>
+  );
+}
